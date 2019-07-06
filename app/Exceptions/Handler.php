7c36e0419bfb2c1dab844898f5b675a9f175d8f5
Clouding <ghost0436@gmail.com>
2019-07-06 10:34:01 +0800

    Implement client exception catch

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * @var array
+     */
+    protected $dontReport = [];
+
+    /**
+     * @param \Exception $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        if ($exception instanceof ClientException) {
+            $contents = $exception->getResponse()->getBody()->getContents();
+            $message = json_decode($contents)->message;
+            echo "\033[01;31m $message \033[0m";
+
+            return;
+        }
+
+        parent::report($exception);
+    }
+}
